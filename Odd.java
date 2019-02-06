55dada74b67f86102ffa76949d1a9b1abbf0d5a9
20mah7 <20mah7@tcl216-14.cs.williams.edu>
2019-02-06 15:17:29 -0500

    added Odd.java

---
+++ Odd.java
@@ -0,0 +1,19 @@
+class Odd {
+ 
+    public static void main(String[] args){
+	double num = 10000000; 
+	for(int i =0; i<num;i++){
+	    if(i%2==1) {
+		System.out.println(i);
+
+	    }
+
+
+	}
+  
+
+
+    }
+
+
+}
